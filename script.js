0cce4e702891589014271b7cbcb6e5106c0167ba
Ria Rajan Alappat <riatreesa@gmail.com>
2020-01-07 13:54:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function add()
+{
+    var a=parseInt(document.getElementById("t1").value);
+    var a=parseInt(document.getElementById("t2").value);
+    var sum=a+b;
+    document.getElementById("t3").value=sum;
+
+
+
+
+
+}
